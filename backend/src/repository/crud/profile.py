f68adac8820cfa0981e02c1824ffdf9a754ec3ef
Josef Leinweber <josef.leinweber@code.berlin>
2023-02-20 12:38:19 +0100

    feat(profile_CRUD): deleted unused imports

--- backend/src/repository/crud/profile.py
+++ backend/src/repository/crud/profile.py
@@ -6,9 +6,9 @@ from sqlalchemy import exc as sqlalchemy_error
 from sqlalchemy.sql import functions as sqlalchemy_functions
 
 from src.models.db.profile import Profile
-from src.models.schema.profile import ProfileInCreate, ProfileInResponse, ProfileInUpdate
+from src.models.schema.profile import ProfileInCreate, ProfileInUpdate
 from src.repository.crud.base import BaseCRUDRepository
-from src.utilities.exceptions.database import EntityAlreadyExists, EntityDoesNotExist
+from src.utilities.exceptions.database import EntityDoesNotExist
 
 
 class ProfileCRUDRepository(BaseCRUDRepository):
